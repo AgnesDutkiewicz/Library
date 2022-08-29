a5974eb3745b97161ea27382ff317b4c23ca9eaf
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-08-29 20:38:11 +0200

    Add Books Controller, Model and views

--- db/migrate/20220829155129_create_books.rb
+++ db/migrate/20220829155129_create_books.rb
@@ -1,9 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-      t.string :title, :null => false
+      t.string :title, null: false
       t.datetime :publication_date
-      t.string :author, :null => false
+      t.string :author, null: false
       t.string :publisher
 
       t.timestamps
