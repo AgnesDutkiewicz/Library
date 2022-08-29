1a96ff2a51e8346fcad4214ae4d4917d9f4bb1f4
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-08-29 20:18:30 +0200

    Add Books to database

---
+++ db/migrate/20220829155129_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :title, :null => false
+      t.datetime :publication_date
+      t.string :author, :null => false
+      t.string :publisher
+
+      t.timestamps
+    end
+  end
+end
