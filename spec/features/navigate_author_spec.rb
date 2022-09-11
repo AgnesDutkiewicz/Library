6a55177577859271301323e0ec115f7323cb08f3
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-09-11 14:44:38 +0200

    Update views with specs

---
+++ spec/features/navigate_author_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'Navigating authors' do
+  let!(:book) { create :book_with_author }
+
+  context 'Authors#index' do
+    it 'allows navigation to authors#show' do
+      visit authors_url
+
+      click_link 'John Tolkien'
+
+      expect(page).to have_text('John Tolkien')
+      expect(page).to have_text('1892')
+    end
+  end
+
+  context 'Authors#show' do
+    it 'allows navigation from to authors#index' do
+      visit author_url(1)
+
+      click_link 'Authors'
+
+      expect(current_path).to eq(authors_path)
+    end
+
+    it "allows navigation to author's books" do
+      visit author_url(1)
+
+      click_link book.title
+
+      expect(current_path).to eq(book_path(book))
+    end
+  end
+end
--- spec/features/navigate_publisher_spec.rb
+++ spec/features/navigate_publisher_spec.rb
@@ -1,21 +1,34 @@
 require 'rails_helper'
 
 describe 'Navigating publishers' do
-  let!(:publisher) { create :publisher }
+  let!(:publisher1) { create :publisher }
+  let!(:book) { create :book_with_author, publisher: publisher1 }
 
-  it 'allows navigation from the listing page to the detail page' do
-    visit publishers_url
+  context 'Publishers#index' do
+    it 'allows navigation to publishers#show' do
+      visit publishers_url
 
-    click_link publisher.name
+      click_link publisher1.name
 
-    expect(current_path).to eq(publisher_path(publisher))
+      expect(current_path).to eq(publisher_path(publisher1))
+    end
   end
 
-  it 'allows navigation from the detail page to the listing page' do
-    visit publisher_url(publisher)
+  context 'Publishers#show' do
+    it 'allows navigation from the detail page to the listing page' do
+      visit publisher_url(publisher1)
 
-    click_link 'Publishers'
+      click_link 'Publishers'
 
-    expect(current_path).to eq(publishers_path)
+      expect(current_path).to eq(publishers_path)
+    end
+
+    it "allows navigation to publisher's book" do
+      visit publisher_url(publisher1)
+
+      click_link book.title
+
+      expect(current_path).to eq(book_path(book))
+    end
   end
 end
