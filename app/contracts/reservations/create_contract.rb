258dd64e1054719bb593b1b0c59f63f2bf5be31c
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-09-20 16:06:03 +0200

    Contracts Update

--- app/contracts/reservations/create_contract.rb
+++ app/contracts/reservations/create_contract.rb
@@ -4,7 +4,7 @@ module Reservations
       required(:book_id).value(:integer)
       required(:user_id).value(:integer)
       required(:status).value(included_in?: [0, 1, 2])
-      required(:date_of_return).value(:date)
+      required(:return_date).value(:date)
     end
   end
 end
