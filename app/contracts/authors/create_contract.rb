08c05e1e23a8f8d95bdd56e4672c8728fb2de6eb
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-09-20 10:22:47 +0200

    Get rid of duplicated contracts

--- app/contracts/authors/create_contract.rb
+++
@@ -1,8 +0,0 @@
-module Authors
-  class CreateContract < Contract
-    params do
-      required(:name).filled(:string)
-      optional(:birth_date).value(:date_time)
-    end
-  end
-end
--- app/contracts/publishers/create_contract.rb
+++
@@ -1,8 +0,0 @@
-module Publishers
-  class CreateContract < Contract
-    params do
-      required(:name).filled(:string)
-      optional(:origin).filled(:string)
-    end
-  end
-end
