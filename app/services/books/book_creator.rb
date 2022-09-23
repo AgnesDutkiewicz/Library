2c48318a66401c691ac81da03047a03cfb940a2a
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-09-23 15:52:58 +0200

    Book creator update

--- app/services/books/book_creator.rb
+++ app/services/books/book_creator.rb
@@ -4,7 +4,7 @@ module Books
       @title = params[:title]
       @publication_date = params[:publication_date]
       @publisher_id = params[:publisher_id]
-      @author_ids = params[:author_ids].map(&:to_i)
+      @author_ids = params[:author_ids]
     end
 
     def call
@@ -16,13 +16,13 @@ module Books
     def execute
       contract = Books::UpdateContract.new
       result = contract.call(title: @title, publication_date: @publication_date, publisher_id: @publisher_id,
-                             author_ids: @author_ids)
+                             author_ids: @author_ids.map(&:to_i))
       create_book if result.success?
     end
 
     def create_book
       @book = Book.create!(title: @title, publication_date: @publication_date, publisher_id: @publisher_id,
-                           author_ids: @author_ids)
+                           author_ids: @author_ids.map(&:to_i))
     end
   end
 end
