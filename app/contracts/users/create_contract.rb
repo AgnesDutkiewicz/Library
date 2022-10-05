ea64ac093d7b94c9bacdf76e8ac644cb1a6e8832
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-10-05 10:31:24 +0200

    User contracts update

--- app/contracts/users/create_contract.rb
+++ app/contracts/users/create_contract.rb
@@ -3,12 +3,15 @@ module Users
     params do
       required(:name).filled(:string)
       required(:email).filled(:string)
-      required(:password_digest).filled(:string)
-      required(:admin).value(false)
+      required(:password).filled(:string)
     end
 
     rule(:email) do
       key.failure('has invalid format') unless /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.match?(value)
+    end
+
+    rule(:admin) do
+      key.failure('new user cant be admin') if value == true
     end
   end
 end
--- app/contracts/users/update_contract.rb
+++ app/contracts/users/update_contract.rb
@@ -4,11 +4,15 @@ module Users
       required(:name).filled(:string)
       required(:email).filled(:string)
       required(:password_digest).filled(:string)
-      required(:admin).value(:bool)
+      required(:user)
     end
 
     rule(:email) do
       key.failure('has invalid format') unless /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.match?(value)
+    end
+
+    rule(:admin) do
+      key.failure('only admin can make new admin') if user.admin? == false
     end
   end
 end
