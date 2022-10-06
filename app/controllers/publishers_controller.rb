f97ea3f4bc91aaae3c01ab7c36c3557a2f1d6667
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-10-06 14:31:45 +0200

    Add publisher policy

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,20 +1,23 @@
 class PublishersController < ApplicationController
-  before_action :require_sign_in, except: [:index, :show]
-  before_action :require_admin, except: [:index, :show]
 
   def index
     @publishers = Publisher.all
+    authorize @publishers
   end
 
   def show
     @publisher = Publisher.find(params[:id])
+    authorize @publisher
   end
 
   def new
     @publisher = Publisher.new
+    authorize @publisher
   end
 
   def create
+    @publisher = Publisher.new
+    authorize @publisher
     service_object = Publishers::Create.new(current_user, publisher_params.to_h)
     result = service_object.call
     if service_object.success?
@@ -22,17 +25,18 @@ class PublishersController < ApplicationController
       redirect_to @publisher, notice: 'Publisher successfully created!'
     else
       service_object.error_messages
-      @publisher = Publisher.new
       render :new
     end
   end
 
   def edit
     @publisher = Publisher.find(params[:id])
+    authorize @publisher
   end
 
   def update
     @publisher = Publisher.find(params[:id])
+    authorize @publisher
     service_object = Publishers::Update.new(current_user, @publisher, publisher_params.to_h)
     service_object.call
     if service_object.success?
