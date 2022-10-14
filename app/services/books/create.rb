31aac1c2f0f9cc25eaa8b8a69073faf4582911a6
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-10-14 17:15:42 +0200

    Add monad for book create service

--- app/services/books/create.rb
+++ app/services/books/create.rb
@@ -1,24 +1,23 @@
 module Books
   class Create < ApplicationService
-    def initialize(user, params)
-      @user = user
+    include Dry::Monads[:result, :do]
+
+    def initialize(params)
       @params = params
       @errors = []
     end
 
     def call
       prepare_params
-      contract_call = Books::UpdateContract.new.call(params)
-      if contract_call.failure?
-        errors << contract_call.errors.to_h
-      else
-        create_book
-      end
+      yield contract_call
+      book = yield create_book
+
+      Success(book)
     end
 
     private
 
-    attr_reader :params, :user, :errors
+    attr_reader :params, :errors
 
     def prepare_params
       parse_array_ids(params, 'author_ids')
@@ -26,8 +25,22 @@ module Books
       parse_date(params, 'publication_date')
     end
 
+    def contract_call
+      contract_call = Books::UpdateContract.new.call(params)
+
+      return Failure(contract_call.errors.to_h) if contract_call.failure?
+
+      Success()
+    end
+
     def create_book
-      Book.create(**params)
+      book = Book.new(**params)
+
+      if book.save
+        Success(book)
+      else
+        Failure(book.error.full_messages)
+      end
     end
   end
 end
